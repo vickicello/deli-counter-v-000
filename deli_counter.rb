bbbe2dc74323b87ff374bc92a3f63dbd826efb2b
Vicki Hamm <vicki.cello@gmail.com>
2018-06-21 13:59:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,28 +1,30 @@
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.size == 0
+def line(array)
+  numbered_array = []
+  if array.size == 0
     puts "The line is currently empty."
   else
     counter = 1
-    katz_deli.each do |name|
-      katz_deli << "#{counter}. #{name}"
+    array.each do |name|
+      numbered_array << "#{counter}. #{name}"
       counter += 1
     end
-    puts "The line is currently: #{katz_deli.join(" ")}"
+    puts "The line is currently: #{numbered_array.join(" ")}"
  end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+def take_a_number(array, name)
+  array << name
+  puts "Welcome, #{name}. You are number #{array.size} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli.size == 0
+def now_serving(array)
+  new_array = []
+  if array.size == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.first}."
-    return katz_deli.shift
+    puts "Currently serving #{array.first}."
+    return array.shift
   end
 end
